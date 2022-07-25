faa24e24a44b94fb65de2479e4c0c62e311f9aa4
no <ianjwuth@gmail.com>
2022-07-24 20:47:16 -0700

    LAPTOP: 2022-07-24 20:47:15

--- MEDIA/SUPP/hexfill.py
+++ MEDIA/SUPP/hexfill.py
@@ -75,6 +75,21 @@ def item_special(num):
     else:
         return answer
 
+def activation_special(num):
+    answer = ""
+    if num == 1:
+        answer = "anyone"
+    if num == 2:
+        answer = "those connected"
+    if num == 3:
+        answer = "thosed othered"
+    if num == 4:
+        answer = "those samed"
+    if num == 5:
+        answer = "some sacrificed"
+    if num == 4:
+        answer = "some event, maybe timed"
+
 def reason_A(num):
     answer = ""
     if num == 1:
@@ -96,3 +111,102 @@ def reason_A(num):
         return double_trouble
     else:
         return answer
+
+def reason_B(num):
+    answer = ""
+    if num == 1:
+        answer = "easy to spot"
+    if num == 2:
+        answer = "local legend"
+    if num == 3:
+        answer = "local superstition"
+    if num == 4:
+        answer = "humanoid-esq"
+    if num == 5:
+        answer = "oddity or resource"
+    if num == 6:
+        answer = item_special(random.randint(1, 6))
+    if num == 7:
+        answer = "battle, birth, death, or injury"
+    if num == 8:
+        double_trouble = reason_A(random.randint(1, 7)) + " " + reason_A(random.randint(1, 7))
+        return double_trouble
+    else:
+        return answer
+
+def material(num):
+    answer = ""
+    if num == 1:
+        answer = "earthwork"
+    if num == 2:
+        answer = "common metal"
+    if num == 3:
+        answer = "rare metal"
+    if num == 4:
+        answer = "petrified organics"
+    if num == 5:
+        answer = "plain stones"
+    if num == 6:
+        answer = "fancy stones"
+    if num == 7:
+        answer = "unidentified / other"
+    if num == 8:
+        double_trouble = material(random.randint(1, 7)) + " " + material(random.randint(1, 7))
+        return double_trouble
+    else:
+        return answer
+
+def use_case(num):
+    answer = ""
+    if num == 1:
+        answer = "artistic or whimsical"
+    if num == 2:
+        answer = "commemerative"
+    if num == 3:
+        answer = "cultural"
+    if num == 4:
+        answer = "decree or punishment"
+    if num == 5:
+        answer = "historic"
+    if num == 6:
+        answer = "religious"
+    if num == 7:
+        answer = item_special(random.randint(1, 6))
+    if num == 8:
+        double_trouble = reason_A(random.randint(1, 7)) + " " + reason_A(random.randint(1, 7))
+        return double_trouble
+    else:
+        return answer
+
+def age(num):
+    answer = ""
+    if num == 1:
+        answer = "prehistoric"
+    if num == 2:
+        answer = "ancient"
+    if num == 3:
+        answer = "old"
+    if num == 4:
+        answer = "young"
+
+def disposition(num):
+    answer = ""
+    if num == 1:
+        answer = "feared"
+    if num == 2:
+        answer = "loved"
+    if num == 3:
+        answer = "rarely known"
+    if num == 4:
+        answer = "worshipped"
+
+def age(num):
+    answer = ""
+    if num == 1:
+        answer = "ruined, defaced, or otherwise unrecognizable"
+    if num == 2:
+        answer = "weathered or overgrown"
+    if num == 3:
+        answer = "fair, restored, or otherwise maintained"
+    if num == 4:
+        answer = "pristine or well maintained"
