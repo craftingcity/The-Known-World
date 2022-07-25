59e29de135f01a2e7fa316ee79ba61fbde9b910b
no <ianjwuth@gmail.com>
2022-07-24 21:28:16 -0700

    LAPTOP: 2022-07-24 21:28:16

--- MEDIA/SUPP/hexfill.py
+++ MEDIA/SUPP/hexfill.py
@@ -9,8 +9,6 @@ def reroll():
     rolls.append(random.randint(1, 8))
     rolls.append(random.randint(1, 4))
     rolls.append(random.randint(1, 4))
-    rolls.append(random.randint(1, 4))
-    rolls.append(random.randint(1, 4))
     return rolls
 
 def item_natural(num):
@@ -60,15 +58,15 @@ def item_fabricated(num):
 def item_special(num):
     answer = ""
     if num == 1:
-        answer = "dimensional rift or gate"
+        answer = "dimensional rift or gate actived by " + activation_special(random.randint(1, 6))
     if num == 2:
-        answer = "hidden space"
+        answer = "hidden space actived by " + activation_special(random.randint(1, 6))
     if num == 3:
-        answer = "magical boon"
+        answer = "magical boon actived by " + activation_special(random.randint(1, 6))
     if num == 4:
-        answer = "magical curse"
+        answer = "magical curse actived by " + activation_special(random.randint(1, 6))
     if num == 5:
-        answer = "teleportation"
+        answer = "teleportation actived by " + activation_special(random.randint(1, 6))
     if num == 6:
         double_trouble = item_special(random.randint(1, 5)) + " " + item_special(random.randint(1, 5))
         return double_trouble
@@ -87,8 +85,9 @@ def activation_special(num):
         answer = "those samed"
     if num == 5:
         answer = "some sacrificed"
-    if num == 4:
+    if num == 6:
         answer = "some event, maybe timed"
+    return answer
 
 def reason_A(num):
     answer = ""
@@ -123,7 +122,7 @@ def reason_B(num):
     if num == 4:
         answer = "humanoid-esq"
     if num == 5:
-        answer = "oddity or resource"
+        answer = "oddities or resources"
     if num == 6:
         answer = item_special(random.randint(1, 6))
     if num == 7:
@@ -151,7 +150,7 @@ def material(num):
     if num == 7:
         answer = "unidentified / other"
     if num == 8:
-        double_trouble = material(random.randint(1, 7)) + " " + material(random.randint(1, 7))
+        double_trouble = material(random.randint(1, 7)) + "& " + material(random.randint(1, 7))
         return double_trouble
     else:
         return answer
@@ -173,7 +172,7 @@ def use_case(num):
     if num == 7:
         answer = item_special(random.randint(1, 6))
     if num == 8:
-        double_trouble = reason_A(random.randint(1, 7)) + " " + reason_A(random.randint(1, 7))
+        double_trouble = reason_A(random.randint(1, 7)) + " & " + reason_A(random.randint(1, 7))
         return double_trouble
     else:
         return answer
@@ -188,6 +187,7 @@ def age(num):
         answer = "old"
     if num == 4:
         answer = "young"
+    return answer
 
 def disposition(num):
     answer = ""
@@ -199,8 +199,9 @@ def disposition(num):
         answer = "rarely known"
     if num == 4:
         answer = "worshipped"
+    return answer
 
-def age(num):
+def erosion(num):
     answer = ""
     if num == 1:
         answer = "ruined, defaced, or otherwise unrecognizable"
@@ -210,3 +211,37 @@ def age(num):
         answer = "fair, restored, or otherwise maintained"
     if num == 4:
         answer = "pristine or well maintained"
+    return answer
+
+def main():
+    bag_o_dice = reroll()
+    final_answer = "This hex is occupied by a "
+    if bag_o_dice[0] > 1:
+        final_answer += "natural "
+        final_answer += item_natural(bag_o_dice[1])
+        final_answer += ". The reason this place is significat is because it is/has "
+        final_answer += reason_A(bag_o_dice[2])
+        final_answer += ". This place is "
+        final_answer += age(bag_o_dice[5])
+        final_answer += "and "
+        final_answer += disposition(bag_o_dice[6]) + "."
+    else:
+        final_answer += "fabricated "
+        final_answer += item_fabricated(bag_o_dice[1])
+        final_answer += ". The reason this place is significat is because it is/has "
+        final_answer += reason_B(bag_o_dice[2])
+        final_answer += ". It is made of some type of " + material(bag_o_dice[3])
+        final_answer += " and it is constructed for some " + use_case(bag_o_dice[4])
+        final_answer += " purpose. This place is "
+        final_answer += age(bag_o_dice[5])
+        final_answer += " and "
+        final_answer += disposition(bag_o_dice[6]) + "."
+    print(final_answer)
+    grab = input()
+    if grab == "r":
+        main()
+    else:
+        return
+
+if __name__ == "__main__":
+    main()
