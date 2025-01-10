a08191be5369db8bd246ec24e70c1d403b555a4b
craftingcity <78283256+craftingcity@users.noreply.github.com>
2025-01-09 21:43:07 -0800

    LAPTOP: 2025-01-09 21:43:07

--- MEDIA/SUPP/SCRIPTS/price_calculator.py
+++ MEDIA/SUPP/SCRIPTS/price_calculator.py
@@ -4,6 +4,87 @@ selectedMaterial = 0
 isCompatable = False
 
 ### CLASS
+##### BASE
+class BaseItem:
+    def __init__(self, name, weight, craftTime, complexity):
+        self.name = name                #   name of specific item
+        self.weight = weight            #   weight in lbs per item
+        self.craftTime = craftTime      #   time in hours per item
+        self.complexity = complexity    #   complexity in silver paid per hour
+        pass
+
+class BaseMaterial:
+    def __init__(self, name, cost):
+        self.name = name                #   name of specific item
+        self.cost = cost                #   silver per lb of material
+        pass
+
+##### ITEMS
+class TypeArmor(BaseItem):
+    def __init__(self, name, weight, craftTime, complexity, armorclass, armortype, strengthreq, stealthbonus):
+        super().__init__(name, weight, craftTime, complexity)
+        self.armorclass = armorclass    # str
+        self.armortype = armortype      # str
+        self.strengthreq = strengthreq  # int
+        self.stealthbonus = stealthbonus    # str
+        pass
+
+class TypeMelee(BaseItem):
+    def __init__(self, name, weight, craftTime, complexity, wepdamage, wepstat):
+        super().__init__(name, weight, craftTime, complexity)
+        self.wepdamage = wepdamage      # "xdy"
+        self.wepstat = wepstat          # STR/DEX
+        pass
+
+class TypeRange(BaseItem):
+    def __init__(self, name, weight, craftTime, complexity, wepdamage, ammunition, firearm=False):
+        super().__init__(name, weight, craftTime, complexity)
+        self.wepdamage = wepdamage      # "xdy"
+        self.ammunition = ammunition    # str
+        self.firearm = firearm          # bol
+        pass
+
+class TypeCasting(BaseItem):
+    def __init__(self, name, weight, craftTime, complexity, knownspells, charges=0):
+        super().__init__(name, weight, craftTime, complexity)
+        self.knownspells = knownspells  # str
+        self.charges = charges          # int
+        pass
+
+class TypeTools(BaseItem):
+    def __init__(self, name, weight, craftTime, complexity, charges=0):
+        super().__init__(name, weight, craftTime, complexity)
+        self.charges = charges          # int
+        pass
+
+class TypeProduct(BaseItem):
+    def __init__(self, name, weight, craftTime, complexity, use, charges=0):
+        super().__init__(name, weight, craftTime, complexity)
+        self.use = use                  # str
+        self.charges = charges          # int
+        pass
+
+##### MATERIALS
+class TypeTextile(BaseMaterial):
+    def __init__(self, name, cost):
+        super().__init__(name, cost)
+
+class TypeMetalic(BaseMaterial):
+    def __init__(self, name, cost):
+        super().__init__(name, cost)
+
+class TypeGem(BaseMaterial):
+    def __init__(self, name, cost):
+        super().__init__(name, cost)
+
+class TypeFlora(BaseMaterial):
+    def __init__(self, name, cost):
+        super().__init__(name, cost)
+
+class TypeFauna(BaseMaterial):
+    def __init__(self, name, cost):
+        super().__init__(name, cost)
+
 
 ### LOCAL DEF
 
@@ -31,8 +112,32 @@ def TellItemCategories():
     pass
 
 def TellMaterialCategories():
-    print("Textile, Mettalic, ")
+    print("(T)extiles, (M)etalics, (G)emstones, Fl(o)ra, Fa(u)na")
+    answer = input()
+    match answer:
+        case "t":
+            pass
+        case "m":
+            pass
+        case "g":
+            pass
+        case "o":
+            pass
+        case "u":
+            pass
+        case _:
+            pass
     pass
+
+def MakeBaseItems():
+    pass
+
+def MakeBaseMaterial():
+    pass
+
+def GenerateBases():
+    MakeBaseItems()
+    MakeBaseMaterial()
 
 def Start():
     print("Looking at (I)tems or (M)aterials?")
@@ -48,6 +153,7 @@ def Start():
 ### MAIN
 
 def main():
+    GenerateBases()
     Start()
     pass
 
