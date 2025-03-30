fd2c47bb7b02dbacb0f177e6f74cb7255c690028
craftingcity <78283256+craftingcity@users.noreply.github.com>
2025-03-29 22:24:13 -0700

    LAPTOP: 2025-03-29 22:24:13

---
+++ MEDIA/SUPP/SCRIPTS/motifdisplay.py
@@ -0,0 +1,39 @@
+AlchemicalMotifs = [
+    "earth",
+    "water",
+    "fire",
+    "air",
+    "healing",
+    "disease",
+    "illusion",
+    "light",
+    "arcana",
+    "weight",
+    "form",
+    "poison",
+    "desist",
+    "divine",
+    "regenerate",
+    "necrot",
+    "degenerate",
+    "fabrim",
+    "charm",
+    "evoke",
+    "immute",
+    "dunam",
+    "immort",
+    "dragon",
+    "mortal",
+    "abjure",
+    "bane",
+    "spirit"
+]
+
+combonations = 0
+
+for FirstMotif in AlchemicalMotifs:
+    sayFirst = FirstMotif
+    for SecondMotif in AlchemicalMotifs:
+        saySecond = SecondMotif
+        combonations += 1
+        print(f"{combonations}; {sayFirst} {saySecond}")
